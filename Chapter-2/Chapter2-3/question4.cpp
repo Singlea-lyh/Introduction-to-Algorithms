cf475d607dcd70246efca7fbe37ac408c79249be
Singlea-lyh <chinese_lyh@163.com>
2018-05-29 20:17:00 +0800

    correction chapter2-3 question4.txt

--- Chapter-2/Chapter2-3/question4.cpp
+++ Chapter-2/Chapter2-3/question4.cpp
@@ -1,6 +1,6 @@
 /*# 给出一个确定n个元素任意排列中逆序对数量的算法，时间复杂度为nlgn
 *
-*算法描述：采用归并排序的思想，在最后合并时，比较数组中元素大小，计算逆序对数，每次合并时，前面数组元素小于后面数组，则逆序对数加一
+*算法描述：采用归并排序的思想，在最后合并时，比较数组中元素大小，计算逆序对数，每次合并时，前面数组元素小于后面数组，说明L中后续元素都比R中后续元素大，此时逆序对数加上L中i之后还剩元素个数
 
 *伪代码：
 *   globalInversion = 0                     // 记录逆序对数 
@@ -17,7 +17,7 @@
 *            if L[i] > R[j] 
 *                A[k] = R[j]
 *                ++j
-*                ++globalInversion
+*                globalInversion += n1 - i     // 
 *            else 
 *                A[k] = L[i]
 *                ++i
@@ -30,10 +30,11 @@
 *            
 */ 
 #include <iostream>
+#define MAX_VALUE 12345678
 using namespace std;
-#if 0
+#if 1             // 定义全局变量完成统计
 int globalInversion = 0;
-int  INVERSIONMERGER(int A[], int left, int mid, int right)
+void  INVERSIONMERGER(int A[], int left, int mid, int right)
 {
 	int i, j, k;
 	int n1 = mid - left + 1;
@@ -55,7 +56,7 @@ int  INVERSIONMERGER(int A[], int left, int mid, int right)
 		{
 			A[k] = R[j++];
 			// cout<< ++globalInversion << " : " << "<" << L[i] << "," << R[j] << ">" << endl;
-			globalInversion = n1 - i;
+			globalInversion  += n1 - i;
 		}
 		else 
 		{
@@ -64,44 +65,131 @@ int  INVERSIONMERGER(int A[], int left, int mid, int right)
 	    if(i >= n1 || j >= n2)
 		  break;
 	}
+	
 	while(i < n1)
 	  A[++k] = L[i++];
 	while(j < n2)
 	  A[++k] = R[j++];
+
+	delete[] L, R;
+/*
 	for(int l = 0; l < 5; l++)
 	  cout<< A[l] << " ";
 	cout<<endl;
-	return globalInversion;
+*/ 
+	return;
  }
 
-int  MERGERSORT(int A[], int left, int right)
+void  MERGERSORT(int A[], int left, int right)
 {
 	int mid = (right + left) / 2;
 	int nleft , nright , count;
-	if(left < right)
-	{
-	   nleft = MERGERSORT(A, left, mid);
-	   nright = MERGERSORT(A, mid + 1, right );
-	   count = nleft + nright +INVERSIONMERGER(A, left, mid, right);
-	} 
-	
+	if(left >= right)
+	  return ;
+    count = 0;
+
+	MERGERSORT(A, left, mid);
+	MERGERSORT(A, mid + 1, right );
+	INVERSIONMERGER(A, left, mid, right); 
+/*	
     for(int i = 0; i < 5; ++i)
 	  cout<< A[i] << " ";
 	cout<<endl;
-    cout << count << endl;
-	return count;
+*/ 
+  //  cout << count << endl;
+	return;
 }
 
 int main()
 {
 	int A[5] = {2, 3, 8, 6, 1};
 	MERGERSORT(A, 0, 4);
+	cout<< globalInversion  << endl;
 	return 0;
 }
 #endif
 
 
-#if 1
+#if 0                        // 自己完成 
+int  INVERSIONMERGER(int A[], int left, int mid, int right)
+{
+	int i, j, k;
+	int inversion = 0;
+	int n1 = mid - left + 1;
+	int n2 = right - mid;
+	int* L = new int[n1];
+	int* R = new int[n2];
+	for(i = 0; i < n1; ++i)
+	{
+		L[i] = A[left + i];
+	}
+	for(j = 0; j < n2; ++j)
+	{
+		R[j] = A[mid + j + 1];
+	}
+	i = 0; j = 0;
+	for(k = left; k <= right; ++k)
+	{
+		if(L[i] > R[j])
+		{
+			A[k] = R[j++];
+			// cout<< ++globalInversion << " : " << "<" << L[i] << "," << R[j] << ">" << endl;
+			inversion += n1 - i;
+		}
+		else 
+		{
+			A[k] = L[i++];
+		}
+	    if(i >= n1 || j >= n2)
+		  break;
+	}
+	
+	while(i < n1)
+	  A[++k] = L[i++];
+	while(j < n2)
+	  A[++k] = R[j++];
+
+	delete[] L, R;
+/*
+	for(int l = 0; l < 5; l++)
+	  cout<< A[l] << " ";
+	cout<<endl;
+*/ 
+	return inversion;
+ }
+
+int  MERGERSORT(int A[], int left, int right)
+{
+	int mid = (right + left) / 2;
+	int nleft , nright , count;
+	if(left >= right)
+	  return 0;
+    count = 0;
+
+	nleft = MERGERSORT(A, left, mid);
+	nright = MERGERSORT(A, mid + 1, right );
+	count = nleft + nright +INVERSIONMERGER(A, left, mid, right); 
+/*	
+    for(int i = 0; i < 5; ++i)
+	  cout<< A[i] << " ";
+	cout<<endl;
+*/ 
+  //  cout << count << endl;
+	return count;
+}
+
+int main()
+{
+	int A[5] = {2, 3, 8, 6, 1};
+    int count = 0;
+	count = MERGERSORT(A, 0, 4);
+	cout<< count << endl;
+	return 0;
+}
+#endif
+
+
+#if 0                 // 网上
 #define MAX_VALUE 12343564
 int Count_Reversations(int *A, int n);
 int Merge_Reversations(int *A, int p, int r);
